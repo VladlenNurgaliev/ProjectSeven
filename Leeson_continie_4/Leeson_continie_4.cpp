ccdd0ec86a81df559638d4b9175deeab1710ef94
VladlenNurgaliev <vlad.nurgalier@mail.ru>
2022-12-22 21:39:16 +0300

    ProjectSeven

---
+++ Leeson_continie_4/Leeson_continie_4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include "pugixml.hpp"
+using namespace std;
+
+
+
+int main()
+{
+	pugi::xml_document			doc;
+	pugi::xml_parse_result		result = doc.load_file("C:\\Users\\User\\source\\repos\\Lesson_Continie_3\\Leeson_continie_4\\File C++.xml");
+	
+	if (result.status == pugi::status_file_not_found)
+	{
+		cerr << "ERROR" << endl;
+		exit(1);
+	}
+
+	string element1 = doc.child("root").child("one").text().as_string();
+	string element2 = doc.child("root").child("two").text().as_string();
+	string element3 = doc.child("root").child("three").text().as_string();
+
+	cout << element1 << endl;
+	cout << element2 << endl;
+	cout << element3 << endl;
+	
+
+
+	return 0;
+}
---
+++ Lesson_Continie_3/Lesson_Continie_3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+class A
+{
+private:
+    int *a;
+public:
+    A()
+    {
+        a = new int[20];
+    }
+
+    A(const A& other)
+    {
+        a = new int[20];
+
+        for (int i = 0; i < 20; i++)
+        {
+            a[i] = other.a[i];
+        }
+    }
+
+    ~A()
+    {
+        delete []a;
+    }
+};
+
+int main()
+{
+    A a;
+    A b = a;
+    //A b = a;
+
+    return 0;
+
+}
